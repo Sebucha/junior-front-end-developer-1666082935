03cf1db4b071685ea231d88c5ca90a86ed2a3af8
SEBASTIAN <seba4848@vp.pl>
2022-10-19 18:47:51 +0200

    feat(taskslist): updated styles

--- src/components/db.js
+++ src/components/db.js
@@ -1,44 +1,37 @@
-// const colors = ["green", "blue", "gray"]
-
 const taskList = [
     {
         id: 0,
-        title:"Application Setup",
-        state:true,
-        // color: colors[1],
+        title: "Application Setup",
+        state: true,
         bussinesContext: []
     },
     {
         id: 1,
-        title:" Static Books List",
-        state:false,
-        // color: colors[2],
+        title: " Static Books List",
+        state: false,
         bussinesContext: []
 
     },
     {
         id: 2,
-        title:"Administration Panel",
-        state:false,
-        // color: colors[2],
+        title: "Administration Panel",
+        state: false,
         bussinesContext: []
 
     },
     {
         id: 3,
-        title:"Connect Admin with Frontend",
-        state:false,
-        // color: colors[2],
+        title: "Connect Admin with Frontend",
+        state: false,
         bussinesContext: []
 
     },
     {
         id: 4,
-        title:"Book Review Feature",
-        state:false,
-        // color: colors[2],
+        title: "Book Review Feature",
+        state: false,
         bussinesContext: []
     },
-    
+
 ]
 export default taskList
--- src/components/tasksList.js
+++ src/components/tasksList.js
@@ -1,5 +1,4 @@
 import "./tasksList.css"
-import "./colors.css"
 
 import React, { useState } from "react"
 
@@ -7,29 +6,18 @@ import ListingTasks from "./listingTasks"
 
 export default function TasksList() {
 
-    const completed = <i class="fa-solid fa-arrow-up green"></i>;
-    const active = <i class="fa-solid fa-arrow-right blue"></i>;
-    const blocked = <i class="fa-sharp fa-solid fa-lock gray"></i>;
-
-    const [isActive, setActive] = useState(false)
-    const [icon, setIcon]= useState(blocked)
+    const completedIcon = <i class="fa-solid fa-arrow-up green"></i>;
+    const activeIcon = <i class="fa-solid fa-arrow-right blue"></i>;
+    const blockedIcon = <i class="fa-sharp fa-solid fa-lock gray"></i>;
     
 
-    function handleClick() {
-        setActive(!isActive)
-    }
-    function changeIcon(){
-        setIcon(active)
-    }
-   
 
     return (
         <div className="container">
             <header className="header">Your TASKS</header>
             <div className="tasksList">
-                <button onClick={handleClick}> Change state</button>
-                <span className={isActive ? 'blue': "gray"}>
-                    <span>{icon}</span>
+                {/* <button onClick={handleClick}> Change state</button> */}
+                <span >
                     <ListingTasks></ListingTasks>
                 </span>
             </div>
